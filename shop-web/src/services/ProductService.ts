109f930eb66bbec6eebc28cfbe82b5e00bf1dc80
Numpon Maytangsuphut <numpon014@gmail.com>
2019-08-15 01:52:03 +0700

    finished sort_by function

--- shop-web/src/services/ProductService.ts
+++ shop-web/src/services/ProductService.ts
@@ -1,21 +1,22 @@
 import ApiService from './Service';
 
 const ProductService = {
-  async getAllProducts(page: number) {
-    const requestData = {
-      method: 'get',
-      url: `/categories/11/products?page=${page}&sort=price`,
-    };
+    // tslint:disable-next-line:max-line-length
+    async getAllProducts({page = 1, sort_column = 'name', sort_direction = 'asc'}: { page: number; sort_column: string, sort_direction: string }) {
+        const requestData = {
+            method: 'get',
+            url: `/categories/11/products?page=${page}&sort=${sort_column}&direction=${sort_direction}`,
+        };
 
-    try {
-      const response = await ApiService.customRequest(requestData);
-      return response.data;
-    } catch (error) {
-      throw new Error(error.response);
-    }
-  },
+        try {
+            const response = await ApiService.customRequest(requestData);
+            return response.data;
+        } catch (error) {
+            throw new Error(error.response);
+        }
+    },
 };
 
 export default ProductService;
 
-export { ProductService };
+export {ProductService};
--- shop-web/src/views/ProductPage.vue
+++ shop-web/src/views/ProductPage.vue
@@ -1,16 +1,34 @@
 <template>
-    <b-container class="bv-example-row">
-        <ProductList
-                v-if='isFetchProductsSuccess'
-                :products='products'
-        ></ProductList>
-        <b-pagination size="md"
-                      :total-rows="totalItems"
-                      :per-page="perPage"
-                      v-model="currentPage"
-                      @input="fetchProductList(currentPage)">
-        </b-pagination>
-    </b-container>
+    <div class="product-page">
+        <b-container class="bv-example-row-flex-cols">
+            <b-row class="sort-by mt-3">
+                <b-col md="3" offset-md="9" align-self="end">
+                    <div class="float-left">
+                        <span class="align-middle">Sort by</span>
+                    </div>
+                    <div class="float-right">
+                        <b-form-select v-model="sorting.selected"
+                                       v-on:change="onSort"
+                                       :options="sorting.options"
+                                       size="sm"></b-form-select>
+                    </div>
+                </b-col>
+            </b-row>
+        </b-container>
+        <b-container>
+            <b-spinner label="Spinning" v-if='!isFetchProductsSuccess'></b-spinner>
+            <ProductList
+                    v-if='isFetchProductsSuccess'
+                    :products='products'
+            ></ProductList>
+            <b-pagination size="md"
+                          :total-rows="pagination.totalItems"
+                          :per-page="pagination.perPage"
+                          v-model="pagination.currentPage"
+                          @input="fetchProductList(pagination.currentPage)">
+            </b-pagination>
+        </b-container>
+    </div>
 </template>
 
 <script>
@@ -26,19 +44,52 @@
       return {
         isFetchProductsSuccess: false,
         products: [],
-        currentPage: 1,
-        perPage: 20,
-        totalItems: 0
+        pagination: {
+          currentPage: 1,
+          perPage: 20,
+          totalItems: 0,
+        },
+       sorting: {
+         selected: 'name-asc',
+         options: [
+           { value: 'name-asc', text: 'Name A-Z' },
+           { value: 'name-desc', text: 'Name Z-A' },
+           { value: 'price-asc', text: 'Price: Low to High' },
+           { value: 'price-desc', text: 'Price: High to Low' },
+         ]
+       }
       };
     },
     methods: {
       fetchProductList(page) {
-        ProductService.getAllProducts(page).then((response) => {
+        ProductService.getAllProducts({ page }).then((response) => {
           this.products = response.products;
-          this.totalItems = response.meta.total_count;
+          this.pagination.totalItems = response.meta.total_count;
           this.isFetchProductsSuccess = true;
         });
       },
+      onSort(e) {
+        let sort_column = "name";
+        let sort_direction = "asc";
+        switch (e) {
+          case "name-desc":
+            sort_direction = "asc";
+            break;
+          case "price-asc":
+            sort_column = "price";
+            sort_direction = "asc";
+            break;
+          case "price-desc":
+            sort_column = "price";
+            sort_direction = "desc";
+            break;
+        }
+        ProductService.getAllProducts({ sort_column, sort_direction }).then((response) => {
+          this.products = response.products;
+          this.pagination.totalItems = response.meta.total_count;
+          this.isFetchProductsSuccess = true;
+        });
+      }
     },
     created() {
       this.fetchProductList(1);
