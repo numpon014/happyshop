24acc782965b74da7b70008689c8d272628422ed
Numpon Maytangsuphut <numpon014@gmail.com>
2019-08-16 01:24:45 +0700

    fixed test in model

--- shop-api/spec/models/category_spec.rb
+++ shop-api/spec/models/category_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   # Association test
-  it { should have_many(:products).dependent(:destroy) }
+  it { should have_and_belong_to_many(:products) }
 
   # Validation tests
   it { should validate_presence_of(:name) }
--- shop-api/spec/models/product_spec.rb
+++ shop-api/spec/models/product_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  it { should belong_to(:category) }
+  it { should have_and_belong_to_many(:categories) }
 
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:price) }
