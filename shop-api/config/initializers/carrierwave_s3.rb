9c398fbd5de4ceb60fea62d3a2415279731686bb
Numpon Maytangsuphut <numpon014@gmail.com>
2019-08-18 02:13:55 +0700

    fixed s3

--- shop-api/config/initializers/carrierwave_s3.rb
+++ shop-api/config/initializers/carrierwave_s3.rb
@@ -5,6 +5,7 @@ CarrierWave.configure do |config|
 
   config.aws_credentials = {
       access_key_id: ENV["S3_ACCESS_KEY"],
-      secret_access_key: ENV["S3_SECRET_KEY"]
+      secret_access_key: ENV["S3_SECRET_KEY"],
+      region: 'ap-southeast-1'
   }
 end
